0ff751727312b098672c99011bfd044d744f6928
Vinicius Alves <vinicius.araujo1239@gmail.com>
2023-04-06 23:01:55 -0300

    Exercicios

---
+++ Lista-02/Ex01/atividade1.js
@@ -0,0 +1,33 @@
+var reajuste = 10
+
+var SalarioAtualPessoa1 = 2000 
+var SalarioAtualPessoa2 = 5000
+var SalarioAtualPessoa3 = 1000
+var SalarioAtualPessoa4 = 3000
+var SalarioAtualPessoa5 = 6000
+
+var ValorReajustePessoa1 = SalarioAtualPessoa1 * reajuste / 100 
+var SalarioReajustadoPessoa1 = SalarioAtualPessoa1 + ValorReajustePessoa1
+
+var ValorReajustePessoa2 = SalarioAtualPessoa2 * reajuste / 100 
+var SalarioReajustadoPessoa2 = SalarioAtualPessoa2 + ValorReajustePessoa2
+
+var ValorReajustePessoa3 = SalarioAtualPessoa3 * reajuste / 100 
+var SalarioReajustadoPessoa3 = SalarioAtualPessoa3 + ValorReajustePessoa3
+
+var ValorReajustePessoa4 = SalarioAtualPessoa4 * reajuste / 100 
+var SalarioReajustadoPessoa4 = SalarioAtualPessoa4 + ValorReajustePessoa4
+
+var ValorReajustePessoa5 = SalarioAtualPessoa5 * reajuste / 100 
+var SalarioReajustadoPessoa5 = SalarioAtualPessoa5 + ValorReajustePessoa5
+
+console.log("Miguel ganha R$" + SalarioAtualPessoa1 + ", com reajuste de " + reajuste + "% passa a ganhar R$"
+             + SalarioReajustadoPessoa1 )
+console.log("Mateus ganha R$" + SalarioAtualPessoa2 + ", com reajuste de " + reajuste + "% passa a ganhar R$"
+             + SalarioReajustadoPessoa2 )
+console.log("Davi ganha R$" + SalarioAtualPessoa3 + ", com reajuste de " + reajuste + "% passa a ganhar R$"
+             + SalarioReajustadoPessoa3 )
+console.log("Jonas ganha R$" + SalarioAtualPessoa4 + ", com reajuste de " + reajuste + "% passa a ganhar R$"
+             + SalarioReajustadoPessoa4 )
+console.log("Francisco ganha R$" + SalarioAtualPessoa5 + ", com reajuste de " + reajuste + "% passa a ganhar R$"
+             + SalarioReajustadoPessoa5 )
